c6d4d73f565876d130c390465de59214235059dc
andywa <biom4000@yahoo.com.tw>
2018-02-01 22:38:41 +0800

    02011038

--- js/favorites.js
+++ js/favorites.js
@@ -1,61 +1,66 @@
-var map;
+// This sample uses the Place Autocomplete widget to allow the user to search
+// for and select a place. The sample then displays an info window containing
+// the place ID and other information about the place that the user has
+// selected.
+
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
 function initMap() {
-    if (navigator.geolocation) {
+    if (navigator.geolocation)
+    {
         // HTML5 定位抓取
         navigator.geolocation.getCurrentPosition(function (position) {
-                mapServiceProvider(position.coords.latitude, position.coords.longitude);
-            },
-            function(error) {
-                switch (error.code) {
-                    case error.TIMEOUT:
-                        alert('連線逾時');
-                        break;
-
-                    case error.POSITION_UNAVAILABLE:
-                        alert('無法取得定位');
-                        break;
-
-                    case error.PERMISSION_DENIED: // 拒絕
-                        alert('想要參加本活動，\n記得允許手機的GPS定位功能喔!');
-                        break;
-
-                    case error.UNKNOWN_ERROR:
-                        alert('不明的錯誤，請稍候再試');
-                        break;
-                }
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: position.coords.latitude, lng: position.coords.longitude},
+                zoom: 13
             });
-    } else { // 不支援 HTML5 定位
-        // 若支援 Google Gears
-        if (window.google && google.gears) {
-            try {
-                // 嘗試以 Gears 取得定位
-                var geo = google.gears.factory.create('beta.geolocation');
-                geo.getCurrentPosition(successCallback, errorCallback, { enableHighAccuracy: true, gearsRequestAddress: true });
-            } catch (e) {
-                alert('定位失敗請稍候再試');
-            }
-        } else {
-            alert('想要參加本活動，\n記得允許手機的GPS定位功能喔!');
-        }
+        });
     }
-}
 
-// 取得 Gears 定位發生錯誤
-function errorCallback(err) {
-    var msg = 'Error retrieving your location: ' + err.message;
-    alert(msg);
-}
 
-// 成功取得 Gears 定位
-function successCallback(p) {
-    mapServiceProvider(p.latitude, p.longitude);
-}
+    var input = document.getElementById('pac-input');
 
-// 顯示經緯度
-function mapServiceProvider(latitude, longitude) {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: latitude, lng: longitude},
-        zoom: 12
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
+
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    var infowindow = new google.maps.InfoWindow();
+    var marker = new google.maps.Marker({
+        map: map
     });
-    //alert('經緯度：' + latitude + ', ' + longitude);
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+
+    autocomplete.addListener('place_changed', function() {
+        infowindow.close();
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            return;
+        }
+
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+        }
+
+        // Set the position of the marker using the place ID and location.
+        marker.setPlace({
+            placeId: place.place_id,
+            location: place.geometry.location
+        });
+        marker.setVisible(true);
+
+        document.getElementById('place-name').textContent = place.name;
+        document.getElementById('place-id').textContent = place.place_id;
+        document.getElementById('place-address').textContent =
+            place.formatted_address;
+        infowindow.setContent(document.getElementById('infowindow-content'));
+        infowindow.open(map, marker);
+    });
 }
