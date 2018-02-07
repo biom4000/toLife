e121946d371b3b2fa8c2a3cce436022f29d95a8a
andywa <biom4000@yahoo.com.tw>
2018-02-07 22:32:22 +0800

    02071032

--- js/favorites.js
+++ js/favorites.js
@@ -8,37 +8,73 @@
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 var map;
 var infowindow;
-
+var countries;
+var autocomplete;
+var countryRestrict = {'country': 'tw'};
+var markers = [];
+var MARKER_PATH = 'https://developers.google.com/maps/documentation/javascript/images/marker_green';
 function initMap() {
     if (navigator.geolocation)
     {
         // HTML5 定位抓取
         navigator.geolocation.getCurrentPosition(function (position) {
-            var pyrmont = {lat: position.coords.latitude, lng: position.coords.longitude};
+            countries = {
+                'tw':{
+                    center: {lat: position.coords.latitude, lng: position.coords.longitude},
+                    zoom: 9
+                }
+            };
             map = new google.maps.Map(document.getElementById('map'), {
-                center: pyrmont,
-                zoom: 15
+                center: countries['tw'].center,
+                zoom: countries['tw'].zoom
             });
         });
+        autocomplete = new google.maps.places.Autocomplete(
+            (
+                document.getElementById('autocomplete')), {
+                types: ['establishment'],
+                componentRestrictions: countryRestrict
+            });
+
+        autocomplete.addListener('place_changed', onPlaceChanged);
+
     }
 }
-function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
+function onPlaceChanged() {
+    var place = autocomplete.getPlace();
+    if (place.geometry) {
+        map.panTo(place.geometry.location);
+        map.setZoom(17);
+        //search();
+        clearMarkers();
+        var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (0 % 26));
+        var markerIcon = MARKER_PATH + markerLetter + '.png';
+        // Use marker animation to drop the icons incrementally on the map.
+
+        markers[0] = new google.maps.Marker({
+            position: place.geometry.location,
+            animation: google.maps.Animation.DROP,
+            icon: markerIcon
+        });
+
+        // If the user clicks a hotel marker, show the details of that hotel
+        // in an info window.
+        //google.maps.event.addListener(markers[0], 'click', showInfoWindow);
+        setTimeout(dropMarker(0), 0 * 100);
+    } else {
+        document.getElementById('autocomplete').placeholder = 'Enter a city';
+    }
+}
+function clearMarkers() {
+    for (var i = 0; i < markers.length; i++) {
+        if (markers[i]) {
+            markers[i].setMap(null);
         }
     }
+    markers = [];
 }
-
-function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-    });
+function dropMarker(i) {
+    return function() {
+        markers[i].setMap(map);
+    };
 }
